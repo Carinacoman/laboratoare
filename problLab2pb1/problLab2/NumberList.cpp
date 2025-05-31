f57cdeffa8de8b5d7ea084e91d953a16237c4c85
Carinacoman <comancarina@gmail.com>
2025-05-31 13:10:55 +0300

    Update NumberList.cpp

--- problLab2pb1/problLab2/NumberList.cpp
+++ problLab2pb1/problLab2/NumberList.cpp
@@ -3,28 +3,29 @@
 
 void NumberList::Init()
 {
-	this->count = 0;
+	this->count = 0; //seteaza nr de elem la 0 
 }
 
 bool NumberList::Add(int x)
 {
-	if (this->count < 10)
+	if (this->count < 10) //adauga nr in lista doar daca nu s a atins limita de 10 elem
 	{
 		this->numbers[this->count] = x;
 		this->count++;
+		return true;
 	}
 	else {
 		return false;
 	}
 }
 
-void NumberList::Sort()
+void NumberList::Sort() //sortare crescatoare
 {
 	for (int i = 0; i < this->count - 1;i++)
 	{
 		for (int j = i + 1; j < this->count;j++)
 		{
-			if (this->numbers[i] > this->numbers[j])
+			if (this->numbers[i] > this->numbers[j]) //bubble sort
 			{
 				int aux = this->numbers[i];
 				this->numbers[i] = this->numbers[j];
@@ -39,7 +40,7 @@ void NumberList::Print()
 	printf("Vectorul: ");
 	for (int i = 0; i < this->count; i++)
 	{
-		printf("%d ", this->numbers[i]);
+		printf("%d ", this->numbers[i]); //afiseaza elem din vector
 	}
 	printf("\n");
 }
