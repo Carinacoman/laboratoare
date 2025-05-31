feb82d7c61f1df4e2775f729254a5de2df386b93
Carinacoman <comancarina@gmail.com>
2025-05-31 12:08:11 +0300

    Update problLab6.cpp

--- problLab6/problLab6.cpp
+++ problLab6/problLab6.cpp
@@ -23,7 +23,7 @@ int main()
 	c.AddCar(new Fiat());
 	c.AddCar(new RangeRover());
 	c.Race();
-	c.ShowFinalRanks(); // it will print the time each car needed to finish the circuit sorted from the fastest car to the slowest.
-	c.ShowWhoDidNotFinish(); // it is possible that some cars don't have enough fuel to finish the circuit
+	c.ShowFinalRanks(); // afiseaza fiecare masina cand termina circuitul de la cel mai rapid
+	c.ShowWhoDidNotFinish(); // afiseaza masinile care nu termina circuitul
 	return 0;
 }
