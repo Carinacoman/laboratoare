140d779a21741eb01920ce82c0e5e977dc552ba1
Carinacoman <comancarina@gmail.com>
2025-05-31 13:16:22 +0300

    Update Student.cpp

--- problLab2pb2/problLab2/Student.cpp
+++ problLab2pb2/problLab2/Student.cpp
@@ -4,22 +4,24 @@
 
 bool Student::valideGrade(int grade)
 {
-	return grade >= 1 and grade <= 10;
+	return grade >= 1 and grade <= 10; //verif daca e nota valida
 }
 
-void Student::SetName(const char* InputName, int InputNameSize)
+void Student::SetName(const char* InputName, int InputNameSize) //alocare dinamica pt numele studentului
 {
 	this->Name = new char[InputNameSize + 1];
 	memcpy(this->Name, InputName, InputNameSize);
 	this->Name[InputNameSize] = '\0';
+	//if(this->Name != nullprt) //sterge numele anterior
+		//delete[] this->Name;
 
 }
 
 const char* Student::GetName()
 {
-	return this->Name;
+	return this->Name; //returneaza pointerul catre numele studentului
 }
-
+//seteaza si returneaza notele la mate, engl, ist
 void Student::SetMath(int number)
 {
 	this->Math = number;
@@ -50,7 +52,7 @@ int Student::GetHis()
 	return this->His;
 }
 
-int Student::Average()
+int Student::Average() //calc media aritmetica
 {
 	return (this->Math + this->Eng + this->His) / 3.0;
 }
